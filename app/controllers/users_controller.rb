1078ccf0a4bcf368ab06df99d9397fce77a70fef
tak-dwc <taku3277@gmail.com>
2021-07-25 09:00:15 +0000

    [finish]debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,18 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+     render :edit
+    else
+     redirect_to user_path(current_user)  
+    end   
   end
 
   def update
       @books = Book.all
       @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user.id), notice: "You have updated user successfully."
+      redirect_to user_path(current_user), notice: "You have updated user successfully."
     else
       render :edit
     end
